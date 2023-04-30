9419c399133da2beae4dd600b6ab7865cb7a09c9
Esmira11 <ismayilova788@gmail.com>
2023-04-30 20:20:27 +0400

    audio effect added

---
+++ index.js
@@ -0,0 +1,8 @@
+const startBtn = document.getElementById('game-btn');
+
+document.addEventListener('DOMContentLoaded',() => {
+    const gallopSound = document.getElementById('horse-gallop');
+    const horseSound = document.getElementById('horse-sound')
+    horseSound.play();
+    gallopSound.play();
+});
